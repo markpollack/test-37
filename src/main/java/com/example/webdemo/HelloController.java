b9eb7d9f66559ab221a87925b8fb63d779b46eb8
Mark Pollack <mpollack@pivotal.io>
2019-10-25 16:03:55 -0400

    fix git prop generation for use in application

--- src/main/java/com/example/webdemo/HelloController.java
+++ src/main/java/com/example/webdemo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@Value("${git.commit.id}")
+	@Value("${git.commit.id:no-git-for-you}")
 	private String commitId;
 
 	@RequestMapping("/")
