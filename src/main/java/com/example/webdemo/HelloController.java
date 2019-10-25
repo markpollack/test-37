dc123e0bc00aa0f0476a68501e472a38fe2f5030
Mark Pollack <mpollack@pivotal.io>
2019-10-25 15:55:35 -0400

    update message in controller to use git commit id

--- src/main/java/com/example/webdemo/HelloController.java
+++ src/main/java/com/example/webdemo/HelloController.java
@@ -1,13 +1,17 @@
 package com.example.webdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	@Value("${git.commit.id}")
+	private String commitId;
+
 	@RequestMapping("/")
 	public String hello() {
-		return "hello gitops v0.0.5 at 2:56 PM!!!";
+		return "hello gitops at 3:55 PM!!!  commitId = [" + commitId + "]";
 	}
 }
